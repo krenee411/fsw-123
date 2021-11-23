c05f2f2f37bcbdb3e9e9f41651139af3cb7b69e6
krenee411 <kelci.morgan@bryanuniversity.edu>
2021-11-22 20:50:56 -0600

    TodoList part1 added

---
+++ week1/Todo-part1/todo-list/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoList from './TodoList'
+
+function App() {
+  return (
+    <>
+      <h1>List of Todos</h1>
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
---
+++ week1/Todo-part1/todo-list/src/TodoList.js
@@ -0,0 +1,33 @@
+
+function TodoList() {
+
+  return (
+
+    <ul>
+      
+      <li>
+        <input type = 'checkbox'/> 
+        <lable> Walk the dog </lable>
+      </li>
+
+      <li>
+      <input type = 'checkbox'/> 
+        <lable> Check the mail</lable>
+      </li>
+
+      <li>
+      <input type = 'checkbox'/> 
+      <lable> Wash uniforms </lable>
+        </li>
+
+      <li>
+      <input type = 'checkbox'/> 
+      <lable> Work on homework </lable>
+      </li>
+
+    </ul>
+   
+  );
+}
+
+export default TodoList;
